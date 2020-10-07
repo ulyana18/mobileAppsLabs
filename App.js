a27810dfd4c48d4ae7dd1be686421fa464e07877
EugeneBezrukau <eugene.bezruk@gmail.com>
2020-10-07 19:59:33 +0300

    feat: add change view

--- App.js
+++ App.js
@@ -2,36 +2,37 @@ import { StatusBar } from 'expo-status-bar';
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, Button, Alert, TouchableOpacity, TextInput } from 'react-native';
 import Form1 from './components/form1';
+import Form2 from './components/form2';
+import Form3 from './components/form3';
 
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state= {
-      isPressed: false,
-      name: '',
-      surname: '',
-      groupNumber: '',
-      faculty: '',
+      view: 1,
     }
   }
 
-  onClick = () => {
-    const buttonState = this.state.isPressed;
-    this.setState({ 
-      isPressed: !buttonState,
-      name: '',
-      surname: '',
-      groupNumber: '',
-      faculty: ''
-    });
-
+  onChangeView = () => {
+    const viewPage = this.state.view < 3 ? this.state.view + 1 : 1;
+    this.setState({ view: viewPage });
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Form1/>
+        { this.state.view === 1 && <Form1/> }
+        { this.state.view === 2 && <Form2/> }
+        { this.state.view === 3 && <Form3/> }
+        <TouchableOpacity
+          onPress={() => this.onChangeView()}
+        >
+          <Text style = {styles.changeViewButton}>
+            Change View
+          </Text>
+        </TouchableOpacity>
+
       </View>
     );
   
@@ -43,6 +44,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'space-around',
   },
+  changeViewButton: {
+    borderWidth: 1,
+    padding: 15,
+    width: 180,
+    marginBottom: 30,
+    borderColor: 'black',
+    // backgroundColor: 'blue',
+    textAlign: 'center',
+    fontSize: 25,
+    color: '#000'
+  }
 });
